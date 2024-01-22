c5d9b6c4056f16350b68b7fda4b6f061bf053a1e
Chloé Touzani <chloe.touzani11@gmail.com>
2024-01-22 12:00:06 +0100

    Ajout des liens dans les images

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/header"
 import Cards from "../../components/cards"
 import Houses from "../../datas/logements"
+import { Link } from "react-router-dom";
 import "./style.scss"
 
 function Home() {
@@ -8,12 +9,15 @@ function Home() {
         <div>
             <Header />
             <div className="cards-grid">
-            {Houses.map((house, index) => (
-            <Cards 
-            key={`${house.name}-${index}`}
-            title={house.title} 
-            cover={house.cover}/>
-            ))}
+                {Houses.map((house, index) => (
+                    <Link to='/house'>
+                        <Cards 
+                            key={`${house.name}-${index}`}
+                            title={house.title} 
+                            cover={house.cover}
+                        />
+                    </Link>
+                ))}
             </div>
         </div>
     )
